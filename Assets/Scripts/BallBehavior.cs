d890d29f85959b7b68ff68ff643a5d2cd6a38e5c
Plaga Enciso <plaga@Plagas-iMac.local>
2018-07-13 19:06:52 -0500

    first

---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour {
+
+    public Transform paddle;
+    bool gameStarted = false;
+    public Rigidbody2D rbBall;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ( !gameStarted ) {
+            float posDiff = paddle.position.x - transform.position.x;
+            transform.position = new Vector3(paddle.position.x - posDiff, paddle.position.y, paddle.position.z);
+            if ( Input.GetMouseButtonDown(0) ) {
+                rbBall.velocity = new Vector2(8, 8);
+                gameStarted = true;
+
+
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DeadZone : MonoBehaviour {
+
+    public Transform ball;
+    public Text scorePlayer;
+    public Text scoreEnemy;
+    int scorePlayerQty = 0;
+    int scoreEnemyQty = 0;
+    // Use this for initialization
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (gameObject.tag == "Izquierdo")
+        {
+            scoreEnemyQty++;
+            updateScoreLabel(scoreEnemy, scoreEnemyQty);
+        }
+        else if (gameObject.tag == "Derecho")
+        {
+            scorePlayerQty++;
+            updateScoreLabel(scorePlayer, scorePlayerQty);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+    void updateScoreLabel(Text label, int score) {
+        label.text = score.ToString();
+    }
+}
